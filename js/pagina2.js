ab3bee24578c21773cfdf11829347fe81fd052cd
Darkaerys <arthurgato2001@gmail.com>
2024-02-05 18:21:37 +0100

    commit200

--- js/pagina2.js
+++ js/pagina2.js
@@ -2,5 +2,6 @@ $(document).ready(function () {
     $(".btn-back").click(function (e) { 
         e.preventDefault();
         location.href="index.html";
+        console.log("arthur por fin lo sabes");
     });
 });
