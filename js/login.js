76cad2807231626e54e183b148bbd027e0f321a3
Darkaerys <arthurgato2001@gmail.com>
2024-02-05 17:07:36 +0100

    commit-mis cambios

--- js/login.js
+++ js/login.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     $(".btn").click(function () { 
 
-        $(".Cambio").html("buenos dias");
+        $(".Cambio").text("buenos dias");
+        console.log("Esto funciona");
 
     });   
 });
