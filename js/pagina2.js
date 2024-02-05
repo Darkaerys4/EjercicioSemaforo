c2f2eb2e85229838c685f1e2a232de98ba9ca84b
Darkaerys <arthurgato2001@gmail.com>
2024-02-05 18:24:39 +0100

    asdfasd

--- js/pagina2.js
+++ js/pagina2.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     $(".btn-back").click(function (e) { 
-        e.preventDefault();
-        location.href="index.html";
-        console.log("arthur por fin lo sabes");
+
     });
 });
